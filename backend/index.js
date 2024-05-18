f92e83b884e51a25e2bbc8de2d936617a9a2389a
Darshan <darshanarkhade@gmail.com>
2024-05-19 01:31:30 +0530

    Add JWT verfication and error handling

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,22 @@ import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 
 import authRoute from "./routes/auth.route.js";
- 
+
+// Load environment variables
 dotenv.config();
+
+// Initialize Express app
 const app = express();
 
+// Constants
 const PORT = process.env.PORT;
-const MONGO_URI =process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.json());
-app.use(cookieParser());
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(cookieParser()); // Parse cookies
 
+// Connect to MongoDB
 const connect = async () => {
   try {
     await mongoose.connect(MONGO_URI);
@@ -23,10 +29,17 @@ const connect = async () => {
   } 
 };
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).send(errorMessage);
+});  
+
+// Routes
 app.use("/api/auth", authRoute);
 
-
-
+// Start server
 app.listen(PORT, () => {
   connect();
   console.log(`Server is running on port ${PORT}`);
