0f728e64d5cfbd991eda3ac23d7d9441e5816113
Darshan <darshanarkhade@gmail.com>
2024-05-21 00:24:18 +0530

    Linked profile page backend to frontend

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
 
-export default function Profile({ username, fullName, problemsSolved }) {
+export default function Profile() {
+
+  const currentUser=JSON.parse(localStorage.getItem("currentUser"));
+  console.log(currentUser);
+
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
+  }
+  if(!currentUser) return (
+    <div className="message-container">
+      <p className="message m-4">
+        Sorry, this page is only accessible to logged-in users. Please 
+        <a href="/login" className="link"> log in </a> 
+        or 
+        <a href="/signup" className="link"> create an account </a> 
+        to view this content.
+      </p>
+    </div>
+
+  );
   return (
     <div className="max-w-md  my-10 mx-auto bg-white shadow-md rounded-lg overflow-hidden">
       {/* User Image */}
@@ -11,9 +32,12 @@ export default function Profile({ username, fullName, problemsSolved }) {
       
       {/* User Information */}
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Darshan Arkhade {fullName}</div>
-        <p className="text-gray-700 text-base mb-2">@king0203 {username}</p>
-        <p className="text-gray-700 text-base">Problems Solved:499 {problemsSolved}</p>
+        <div className="font-bold text-xl mb-2">Name: {currentUser.fullName}</div>
+        <p className="text-gray-700 text-base mb-2">Username: {currentUser.username}</p>
+        <p className="text-gray-700 text-base mb-2">Email: {currentUser.email}</p>
+        <p className="text-gray-700 text-base mb-2">No of Problems solved: {currentUser.problemsSolved ?currentUser.problemsSolved: 0 }</p>
+        <p className="text-gray-700 text-base">Registered on: {formatDate(currentUser.createdAt)}</p>
+        
       </div>
     </div>
   );
